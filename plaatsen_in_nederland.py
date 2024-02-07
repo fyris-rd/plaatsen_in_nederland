a2b6065f6d4abd4a0bc6d4b12ae7aea60d8af001
Remco Bastiaan Jansen <r.b.jansen.uu@gmail.com>
2024-02-07 18:35:23 +0100

    via get en unzip

--- plaatsen_in_nederland.py
+++ plaatsen_in_nederland.py
@@ -1,5 +1,13 @@
+import os
+import zipfile
 import pandas as pd
+import requests
 
+r = requests.get("https://download.geonames.org/export/dump/NL.zip")
+with open("NL.zip", "wb") as f:
+    f.write(r.content)
+with zipfile.ZipFile("NL.zip", "r") as zip_ref:
+    zip_ref.extractall("./")
 df = pd.read_csv("NL.txt", sep="\t", header=None)
 df.columns = [
     "geonameid",
@@ -22,7 +30,12 @@ df.columns = [
     "timezone",
     "modificationdate",
 ]
-# Subset for "populated places" only. See https://www.geonames.org/export/codes.html
+# Subset op "populated places". Zie https://www.geonames.org/export/codes.html
 df = df.loc[df.featurecode == "PPL"]
 df = df.sort_values(by="name").reset_index(drop=True).dropna(axis=1, how="all")
+
+# Cleanup en export
+os.remove("NL.zip")
+os.remove("NL.txt")
+os.remove("readme.txt")
 df.to_csv("./plaatsen_in_nederland.csv")
