b36b9c7b1cd5f309bfd186425aed5755ddb39a1c
Remco Bastiaan Jansen <r.b.jansen.uu@gmail.com>
2024-02-08 10:50:28 +0100

    met export naar geojson

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM python:latest
+FROM osgeo/gdal:ubuntu-small-3.6.3
+RUN apt-get update && apt-get -y install python3-pip --fix-missing
 WORKDIR /app
 COPY requirements.txt /app/
 RUN pip install --no-cache-dir -r requirements.txt
--- plaatsen_in_nederland.py
+++ plaatsen_in_nederland.py
@@ -1,6 +1,8 @@
 import os
 import zipfile
 import pandas as pd
+import geopandas as gpd
+from shapely.geometry import Point
 import requests
 
 r = requests.get("https://download.geonames.org/export/dump/NL.zip")
@@ -33,6 +35,11 @@ df.columns = [
 # Subset op "populated places". Zie https://www.geonames.org/export/codes.html
 df = df.loc[df.featurecode == "PPL"]
 df = df.sort_values(by="name").reset_index(drop=True).dropna(axis=1, how="all")
+
+# GeoDataFame
+geometry = [Point(xy) for xy in zip(df["longitude"], df["latitude"])]
+gdf = gpd.GeoDataFrame(df, geometry=geometry)
+gdf.to_file("plaatsen_in_nederland.geojson", driver="GeoJSON")
 
 # Cleanup en export
 os.remove("NL.zip")
